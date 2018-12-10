100b255bc8f775c66a6626b830e71152bc9807ca
Ander <Ander@192.168.1.40>
2018-12-10 18:50:12 +0100

    Creación ServletLibros

---
+++ ejemploJavaEE/src/main/java/com/ipartek/formacion/controladores/LibrosServlet.java
@@ -0,0 +1,28 @@
+package com.ipartek.formacion.controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/libros")
+public class LibrosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
